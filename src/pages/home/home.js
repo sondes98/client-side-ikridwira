eeb7f9ae1dcc332ba3f6f74cd848faa80f49e40f
sondes ahmed <sondes.ahmed.77@gmail.com>
2023-12-24 16:37:33 +0100

    fixed an ready to build

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-import { Header } from '../../components/header/Header';
-import  { ResidanceCarousel } from '../../components/carousel/carousel';
-import { AppartementSection } from '../../containers/appartementSection';
-import AnnouceContainer from '../../containers/annouceContainer';
-import FooterSection from '../../containers/footerSection';
+import React, { Component } from "react";
+import { Header } from "../../components/header/Header";
+import { Carousel } from "../../components/carousel/carousel";
+import { AppartementSection } from "../../containers/appartementSection";
+import AnnouceContainer from "../../containers/annouceContainer";
+import FooterSection from "../../containers/footerSection";
 
 class Home extends Component {
-render() {
-return (
-    <div >
-        <Header/>
-        <ResidanceCarousel/>
-        <AnnouceContainer/>
-        <AppartementSection/>
-        <FooterSection/>
-    </div>
-)
-}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Carousel />
+        <AnnouceContainer />
+        <AppartementSection />
+        <FooterSection />
+      </div>
+    );
+  }
 }
 
 export default Home;
-
-
